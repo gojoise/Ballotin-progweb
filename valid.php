03c1f41f1e3c8a91b209aa5fd337cb73061745cd
gojoise <mathieu.benard@u-psud.fr>
2021-04-09 18:58:54 +0200

    ajax du name ok, bug sur options

--- valid.php
+++ valid.php
@@ -1,8 +1,15 @@
 <?php 
-$name = $_GET["name"];
-$question = $_GET["question"];
+$name = $_GET["name"]; 
+$question = $_GET["question"]; 
 
-echo($name)
-echo($question)
+echo($name);
+echo($question);
 
+
+//t'occupes pas de ça c'est juste pour que j'ai un exmple d'array de coté :)
+$arrOpt = $_GET["options"]// l'idée c'est de recup ça comme ça ^^ pas besoin si $_GET[options] est ok !
+
+$data=json_encode($name);
+file_put_contents("scrutins.json",$data,FILE_APPEND);
+    
 ?>
