0bfb00e6de14b81e1ff3da28498747fd7ec8dd70
gojoise <mathieu.benard@u-psud.fr>
2021-04-11 19:27:56 +0200

    affiche votants ok!

--- votants.php
+++ votants.php
@@ -6,7 +6,8 @@ $res = array();
 /*Inspiré de 
 https://scoutapm.com/blog/php-json_encode-serialize-php-objects-to-json
 */
-
+$json_string = file_get_contents("scrutins.json");
+$json = json_decode($json_string, true);
 class vot{
 
 }
@@ -14,14 +15,12 @@ for($i=0;$i< count($arrVot);$i=$i+1){
     $vo = new vot();
     $vo->name=$arrVot[$i];
     $vo->nbVotes = $arrProc[$i];
-    array_push($res,$vo);
+    array_push($json["votants"], $vo);
 }
 
-print_r($res);
 
-$json_string = file_get_contents("scrutins.json");
-$json = json_decode($json_string, true);
-array_push($json["votants"], $res);
+
+
 $strNew = json_encode($json);
 file_put_contents("scrutins.json", $strNew);
 
