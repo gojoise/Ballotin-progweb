d47ee869c6a92189aaa20b56b2defc559e7fb7ef
gojoise <mathieu.benard@u-psud.fr>
2021-04-11 19:40:26 +0200

    Nouvelle façon pour scrutins.json

--- valid.php
+++ valid.php
@@ -1,4 +1,5 @@
 <?php 
+$profile=$_GET["profile"];
 $name = $_GET["name"]; 
 $question = $_GET["question"]; 
 $arrOpt = $_GET["options"];
@@ -10,14 +11,21 @@ https://scoutapm.com/blog/php-json_encode-serialize-php-objects-to-json
 class scrutin {
 
 }
+
 $scr= new scrutin();
 $scr->name = $name;
+$scr->organisateur = $profile;
 $scr->question = $question;
 $scr->options = $arrOpt;
 $scr->votants = [];
 
 
-$jsonData = json_encode($scr);
-file_put_contents("scrutins.json",$jsonData,FILE_APPEND);
+$json_string = file_get_contents("scrutins.json");
+$json = json_decode($json_string, true);
+
+array_push($json, $scr);
+
+$strNew = json_encode($json);
+file_put_contents("scrutins.json", $strNew);
     
 ?>
